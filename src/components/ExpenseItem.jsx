182410bc9d99ae4d6951ab29143516ef14c54c97
ferhatozgen <ferhatozgen17@gmail.com>
2025-02-27 23:22:17 +0300

    week 3

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,22 +1,18 @@
+import Card from "./Card";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem({ date, amount, title }) {
-  const month = date.toLocaleDateString("en-US", { month: "long" });
-  const day = date.toLocaleDateString("en-US", { day: "2-digit" });
-  const year = date.getFullYear();
+  
 
   return (
-    <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+    <Card className="expense-item">
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <div>{title}</div>
         <div className="expense-item__price">${amount.toFixed(2)}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
